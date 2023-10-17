2cb36acca391fe9d2ab62a750a169c7d8b728bb6
dinkarsawant <148208227+dinkarsawant@users.noreply.github.com>
2023-10-17 16:03:15 +0530

    Create mathematical conversions.py
    welcome

---
+++ mathematical conversions.py
@@ -0,0 +1,27 @@
+b_num = list(input("Input a binary number: "))
+value = 0
+
+for i in range(len(b_num)):
+	digit = b_num.pop()
+	if digit == '1':
+		value = value + pow(2, i)
+print("The decimal value of the number is", value)
+
+
+
+#program to convert temperature *c to *faradays 
+
+temp = input("Input the  temperature you like to convert? (e.g., 45F, 102C etc.) : ")
+degree = int(temp[:-1])
+i_convention = temp[-1]
+
+if i_convention.upper() == "C":
+  result = int(round((9 * degree) / 5 + 32))
+  o_convention = "Fahrenheit"
+elif i_convention.upper() == "F":
+  result = int(round((degree - 32) * 5 / 9))
+  o_convention = "Celsius"
+else:
+  print("Input proper convention.")
+  quit()
+print("The temperature in", o_convention, "is", result, "degrees.")
